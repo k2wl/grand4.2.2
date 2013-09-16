1737b75a1fb2f9c5c21d20dc6729b3a11cd45117
k2wl <k2wl@yahoo.com>
2013-09-16 10:39:13 +0530

    i9152 support added

--- drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
+++ drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
@@ -2,7 +2,7 @@
 #include <linux/broadcom/vc_debug_sym.h>
 
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "Android-ThinkPad-T400" );
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "1b76dcb8c21132ef88549eb996f5000d6943ae22 (tainted)" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "b15f1899b58d9f21286efdb6dc2a910ecdd5d8ee (tainted)" );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_time,    __TIME__ );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_date,    __DATE__ );
 
